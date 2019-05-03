ad4cc50e0573b2d1eae61f0db4fa6973bcd7884a
Mr. Bartucz <jobartucz@isd535.org>
2019-05-03 07:43:42 -0500

    test URL

--- app/src/main/java/com/ctech/bartucz/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/ctech/bartucz/photogallery/PhotoGalleryFragment.java
@@ -1,16 +1,22 @@
 package com.ctech.bartucz.photogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.IOException;
+
 public class PhotoGalleryFragment extends Fragment {
+
+    private static final String TAG = "PhotoGalleryFragment";
 
     private RecyclerView mPhotoRecyclerView;
 
@@ -22,6 +28,7 @@ public class PhotoGalleryFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+        new FetchItemsTask().execute();
     }
 
     @Nullable
@@ -36,5 +43,19 @@ public class PhotoGalleryFragment extends Fragment {
 
         return v;
     }
+
+    private class FetchItemsTask extends AsyncTask<Void,Void,Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                String result = new FlickrFetchr().getUrlString("https://raytf.org");
+                Log.i(TAG, "Fetched contents of URL: " + result);
+            } catch (IOException ioe) {
+                Log.e(TAG, "Failed to fetch URL: ", ioe);
+            }
+            return null;
+        }
+    }
+
 }
 
