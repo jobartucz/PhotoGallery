2ebaff76de481d714382e9099d3aab1395546b3b
Mr. Bartucz <jobartucz@isd535.org>
2019-05-07 10:08:23 -0500

    adding handlers

--- app/src/main/java/com/ctech/bartucz/photogallery/ThumbnailDownloader.java
+++ app/src/main/java/com/ctech/bartucz/photogallery/ThumbnailDownloader.java
@@ -1,13 +1,20 @@
 package com.ctech.bartucz.photogallery;
 
+import android.os.Handler;
 import android.os.HandlerThread;
 import android.util.Log;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public class ThumbnailDownloader<T> extends HandlerThread {
 
     private static final String TAG = "ThumbnailDownloader";
+    private static final int MESSAGE_DOWNLOAD = 0;
 
     private boolean mHasQuit = false;
+    private Handler mRequestHandler;
+    private ConcurrentMap<T, String> mRequestMap = new ConcurrentHashMap<>();
 
     public ThumbnailDownloader() {
         super(TAG);
