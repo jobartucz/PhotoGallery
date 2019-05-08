e46e239bc07c0f3bf497080164bd6f6b1523829a
Mr. Bartucz <jobartucz@isd535.org>
2019-05-08 07:55:19 -0500

    Added photo handler threads

--- app/src/main/java/com/ctech/bartucz/photogallery/ThumbnailDownloader.java
+++ app/src/main/java/com/ctech/bartucz/photogallery/ThumbnailDownloader.java
@@ -1,9 +1,14 @@
 package com.ctech.bartucz.photogallery;
 
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.os.Message;
 import android.util.Log;
 
+import java.io.IOException;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
@@ -20,6 +25,39 @@ public class ThumbnailDownloader<T> extends HandlerThread {
         super(TAG);
     }
 
+    @SuppressLint("HandlerLeak")
+    @Override
+    protected void onLooperPrepared() {
+        mRequestHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                if (msg.what == MESSAGE_DOWNLOAD) {
+                    T target = (T) msg.obj;
+                    Log.i(TAG, "Got a request for URL: " + mRequestMap.get(target));
+                    handleRequest(target);
+                }
+            }
+        };
+    }
+
+    private void handleRequest(final T target) {
+        try {
+            final String url = mRequestMap.get(target);
+
+            if (url == null) {
+                return;
+            }
+
+            byte[] bitmapBytes = new FlickrFetchr().getUrlBytes(url);
+            final Bitmap bitmap = BitmapFactory
+                    .decodeByteArray(bitmapBytes, 0, bitmapBytes.length);
+            Log.i(TAG, "Bitmap created");
+
+        } catch (IOException ioe) {
+            Log.e(TAG, "Error downloading image", ioe);
+        }
+    }
+
     @Override
     public boolean quit() {
         mHasQuit = true;
@@ -27,6 +65,14 @@ public class ThumbnailDownloader<T> extends HandlerThread {
     }
 
     public void queueThumbnail(T target, String url) {
+
         Log.i(TAG, "Get a URL: " + url);
+
+        if (url == null) {
+            mRequestMap.remove(target);
+        } else {
+            mRequestMap.put(target, url);
+            mRequestHandler.obtainMessage(MESSAGE_DOWNLOAD, target).sendToTarget();
+        }
     }
 }
